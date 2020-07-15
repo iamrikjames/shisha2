37e789db463fd8fdc608e3b680b9d06ed681ff2c
Hendrik Kehres <hendrik.kehres@gmail.com>
2020-07-15 18:34:52 +0200

    theme installation die erste

--- index.php
+++ index.php
@@ -1,17 +1,63 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * Index template.
  *
- * @package WordPress
+ * @package Neve
  */
+$container_class = apply_filters( 'neve_container_class_filter', 'container', 'blog-archive' );
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+get_header();
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+?>
+	<div class="<?php echo esc_attr( $container_class ); ?> archive-container">
+		<div class="row">
+			<?php do_action( 'neve_do_sidebar', 'blog-archive', 'left' ); ?>
+			<div class="nv-index-posts blog col">
+				<?php
+				do_action( 'neve_before_loop' );
+				do_action( 'neve_page_header', 'index' );
+				do_action( 'neve_before_posts_loop' );
+				if ( have_posts() ) {
+					/* Start the Loop. */
+					echo '<div class="posts-wrapper row">';
+
+
+					$pagination_type = get_theme_mod( 'neve_pagination_type', 'number' );
+					if ( $pagination_type !== 'infinite' ) {
+						global $wp_query;
+
+						$posts_on_current_page = $wp_query->post_count;
+						$hook_after_post       = -1;
+
+						if ( $posts_on_current_page >= 2 ) {
+							$hook_after_post = intval( ceil( $posts_on_current_page / 2 ) );
+						}
+						$post_index = 1;
+					}
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'template-parts/content', get_post_type() );
+
+						if ( $pagination_type !== 'infinite' ) {
+							if ( $post_index === $hook_after_post && $hook_after_post !== - 1 ) {
+								do_action( 'neve_middle_posts_loop' );
+							}
+							$post_index ++;
+						}
+					}
+					echo '</div>';
+					if ( ! is_singular() ) {
+						do_action( 'neve_do_pagination', 'blog-archive' );
+					}
+				} else {
+					get_template_part( 'template-parts/content', 'none' );
+				}
+				?>
+				<div class="w-100"></div>
+				<?php do_action( 'neve_after_posts_loop' ); ?>
+			</div>
+			<?php do_action( 'neve_do_sidebar', 'blog-archive', 'right' ); ?>
+		</div>
+	</div>
+<?php
+get_footer();
